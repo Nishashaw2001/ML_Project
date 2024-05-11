074a64448905ff959ce7b0c2765e2a917c2069f4
Nishashaw2001 <147492488+Nishashaw2001@users.noreply.github.com>
2024-05-11 14:40:33 +0530

    All the files are added

---
+++ File1.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+st.title('MPG ML Project')
+
+#displacement = st.number_input('Displacement', value=300, placeholder = 'enter a value for displacement')
+#horsepower = st.number_input('Horsepower', value=130, placeholder = 'enter a value for horsepower')
+#Weight = st.number_input('weight', value=3000, placeholder = 'enter a value for weight')
+#acceleration = st.number_input('acceleration', value= 12, placeholder = 'enter a value for acceleration')
